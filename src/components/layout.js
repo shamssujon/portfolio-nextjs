a72854959e3bbc7cbe15efb6876a8687110306a1
Shamsul Islam Sujon <sujon2762@gmail.com>
2023-03-06 04:50:18 +0600

    Update page layout: contact as footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,12 @@
+import Contact from "./Contact";
 import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
-    return (
-      <>
-        <Navbar />
-        {children}
-      </>
-    )
-  }
+	return (
+		<>
+			<Navbar />
+			{children}
+			<Contact />
+		</>
+	);
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,6 @@ export default function Home() {
 				<Skills />
 				<FeaturedWorks />
 				<Testimonials />
-				<Contact />
 			</main>
 		</>
 	);
